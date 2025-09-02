1127f47b87d1ab632c3e8795df1296e8b0fae1a9
Diyorbek <diyorbek.qodirboyev05@gmail.com>
2025-09-02 21:12:35 +0500

    feat: ⚙️  initialize Flask application with configuration and database setup

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+csrf = CSRFProtect()
+
+def create_app():
+    app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    csrf.init_app(app)
+
+    return app
+
