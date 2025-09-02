c876765a98c1e574a8568a045eab5800d7fc95ae
Diyorbek <diyorbek.qodirboyev05@gmail.com>
2025-09-02 21:39:26 +0500

    feat: 🆕 add User model with authentication methods and update .gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -15,5 +15,10 @@ def create_app():
     login_manager.init_app(app)
     csrf.init_app(app)
 
+    from .models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     return app
 
