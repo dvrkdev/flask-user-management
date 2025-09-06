3d1d9a4203fd43e5257149b2995c0f421a702068
dvrkdev <dvrk.devx@gmail.com>
2025-09-06 21:02:19 +0500

    refactor: 🗑️  remove unused application files and templates for user management

--- app/profile.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('profile', __name__, url_prefix='/profile')
-
-@bp.route('/<int:id>')
-def profile(id):
-    return render_template('profile.html')
